647f8f4fbad91c21dfaf82b582df9d6e15bc1f54
Evandro Goncalves <evandrolvg@gmail.com>
2019-12-27 14:42:38 -0300

    Correção server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-onst express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const nomeApp = process.env.npm_package_name;
 const app = express();
- 
+
 app.use(express.static(`$/dist/$`));
- 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`$/dist/$/index.html`));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(`$/dist/$/index.html`));
 });
- 
+
 app.listen(process.env.PORT || 8080);
